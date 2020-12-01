235efa35d597b5d85518c7e857a58a2f2a170282
Wendy Grant <wendykgrant.tx@gmail.com>
2020-12-01 14:30:49 -0600

    created navbar and header components

---
+++ client/src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import style from "./style.css";
+
+function Header() {
+    return (
+        <div className="jumbotron text-center">
+            <div className="container">
+                <h1>Google Book Search</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
