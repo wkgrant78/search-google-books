066a062891f55c78493d3a797ee47d13699aa964
Wendy Grant <wendykgrant.tx@gmail.com>
2020-12-01 13:42:24 -0600

    created models

---
+++ models/books.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+
+// setup db 
+const bookSchema = new Schema ({
+    _id: { type: String },
+    title: { type: String },
+    authors: { type: Array },
+    description: { type: String },
+    image: { type: String },
+    link: { type: String }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
